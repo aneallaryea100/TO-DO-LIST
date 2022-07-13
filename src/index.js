c42e8c27f2225890b01427e58c815fe8718b680a
Aneal Laryea <aneallaryea100@gmail.com>
2022-07-13 11:16:52 -0700

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import './style.css';
 
-//variables declaration
-const textInput = document.querySelector('input');
+// variables declaration
 const todosMainContain = document.querySelector('.todos-container');
-const clearAllbtn = document.querySelector('.clear-field');
 
 const arrayObject = [
   {
-    description : "Go to the super market",
-    completed : false,
-    index : 1,
+    description: 'Go to the super market',
+    completed: false,
+    index: 1,
   },
   {
-    description : "Go to the Stadium",
-    completed : false,
-    index : 2,
+    description: 'Go to the Stadium',
+    completed: false,
+    index: 2,
   },
   {
-    description : "Submit the assignment",
-    completed : false,
-    index : 3,
+    description: 'Submit the assignment',
+    completed: false,
+    index: 3,
   },
 ];
 
 const displayTemplate = (display) => {
   const todoContainer = document.createElement('div');
-    todoContainer.className = 'todoContainer';
-    todoContainer.innerHTML += `
+  todoContainer.className = 'todoContainer';
+  todoContainer.innerHTML += `
     <p>
     <input type="checkbox" class="checkbox" value = "${display.completed}">
     <span id = "${display.index}">${display.description}</span>
@@ -34,8 +32,8 @@ const displayTemplate = (display) => {
     <i class="fa-solid fa-ellipsis-vertical"></i>
     `;
 
-    return todoContainer;
-}
+  return todoContainer;
+};
 
 arrayObject.forEach((e) => {
   todosMainContain.appendChild(displayTemplate(e));
